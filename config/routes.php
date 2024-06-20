108ab4873ef93926fa350c064de8be539cc2b1b3
Gene Kelley <phpcodehead@gmail.com>
2024-06-20 15:46:29 -0700

    Add create note form and post route.

--- config/routes.php
+++ config/routes.php
@@ -5,5 +5,6 @@ return [
     '/about' => 'controllers/about.php',
     '/notes' => 'controllers/notes.php',
     '/note' => 'controllers/note.php',
+    '/note/create' => 'controllers/note_create.php',
     '/contact' => 'controllers/contact.php',
 ];
