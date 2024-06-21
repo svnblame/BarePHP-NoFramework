7bf76da150db74519a82e612be43fa2c0bb78511
Gene Kelley <phpcodehead@gmail.com>
2024-06-21 11:17:04 -0700

    Refactor to a little more resourceful naming convention.

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -1,6 +1,6 @@
 <?php
 
-$dbConfig = require __DIR__ . '/../config/database.php';
+$dbConfig = require __DIR__ . '/../../config/database.php';
 $db = new Database($dbConfig, 'root', '');
 
 $heading = 'Note';
@@ -14,4 +14,4 @@ $note = $db->query(
 
 authorize($note['user_id'] === $currentUserId);
 
-require 'views/note.view.php';
+require 'views/notes/show.view.php';
