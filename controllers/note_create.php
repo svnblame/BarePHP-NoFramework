4600d6b54493dc0ec51a004a12ae4cf39d5ac7fb
Gene Kelley <phpcodehead@gmail.com>
2024-06-21 08:37:40 -0700

    Add inline form data validation ( will refactor to dedicated Validation class soon ). Add maximum char limits on note body field.

--- controllers/note_create.php
+++ controllers/note_create.php
@@ -7,11 +7,24 @@ $db = new Database($dbConfig, 'root', '');
 $heading = 'Create Note';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $db->query("INSERT INTO notes (body, user_id) VALUES (:body, :user_id)",
-    [
-        'body' => $_POST['body'],
-        'user_id' => $dbConfig['test_user_id']
-    ]);
+
+    $errors = [];
+
+    if (isset($_POST['body']) && 0 === strlen($_POST['body'])) {
+        $errors['body'] = 'A body is required';
+    }
+
+    if (isset($_POST['body']) && strlen($_POST['body']) > $dbConfig['note_body_char_limit']) {
+        $errors['body'] = 'The body of your note is too long. Maximum number of characters is ' . $dbConfig['note_body_char_limit'] . '.';
+    }
+
+    if (empty($errors)) {
+        $db->query("INSERT INTO notes (body, user_id) VALUES (:body, :user_id)",
+            [
+                'body' => $_POST['body'],
+                'user_id' => $dbConfig['test_user_id']
+            ]);
+    }
 }
 
 require __DIR__ . '/../views/note_create.view.php';
