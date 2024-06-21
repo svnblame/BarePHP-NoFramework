5fe65758f221ce3428341b24c35172ab89c1673a
Gene Kelley <phpcodehead@gmail.com>
2024-06-21 10:32:59 -0700

    Add dedicated Validator class. Add min and max allowable chars for body field in config file.

---
+++ helpers/Validator.php
@@ -0,0 +1,15 @@
+<?php
+
+class Validator {
+    public static function string(mixed $value, int $min = 1, int $max = INF): bool
+    {
+        $value = trim($value);
+
+        return strlen($value) >= $min && strlen($value) <= $max;
+    }
+
+    public static function email(string $value)
+    {
+        return filter_var($value, FILTER_VALIDATE_EMAIL);
+    }
+}
