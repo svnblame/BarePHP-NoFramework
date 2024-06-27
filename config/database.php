0ca6a2c201789e06afbd7bb4513d4fc1d3c5d69f
Gene Kelley <phpcodehead@gmail.com>
2024-06-27 16:10:00 -0700

    Add dovenv package. Change database name.

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 return [
     'host'   => 'localhost',
     'port'   => 3306,
-    'dbname' => 'myapp',
+    'dbname' => 'kts',
     'charset' => 'utf8mb4',
     'test_user_id' => 2,
     'note_body_char_max' => 1000,
